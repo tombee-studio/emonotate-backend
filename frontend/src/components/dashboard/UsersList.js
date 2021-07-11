2c6775dd8be9d5174de1ab7e5121ad1fbec8cf42
windows tomoya <tomomail1997@gmail.com>
2021-07-11 19:57:51 +0900

    change api

--- frontend/src/components/dashboard/UsersList.js
+++ frontend/src/components/dashboard/UsersList.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Pagination } from '@material-ui/lab';
+import { Card } from '@material-ui/core';
 
-export default class UsersList extends React.Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
+class UsersList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
   }
+
+  componentDidMount() {
+    fetch('/api/users/?format=json')
+      .then(res => res.json())
+      .then(users => {
+        this.setState({
+          users: users
+        });
+      }); 
+  }
+
+  render() {
+    const { users } = this.state;
+    const handlePaginate = (e, page) => {
+      fetch(`/api/users/?format=json&page=${page}`)
+        .then(res => {
+          return res.json();
+        })
+        .then(users => {
+          this.setState({
+            users: users
+          });
+        });
+    };
+    if(users) {
+      return (
+        <Card>
+          <Pagination 
+            count={users.pagination.total_pages} 
+            variant="outlined" 
+            shape="rounded"
+            onChange={handlePaginate} />
+        </Card>
+      );
+    } else {
+      return (<Card />);
+    }
+  };
 };
+
+export default UsersList;
--- frontend/src/menu/listitems.js
+++ frontend/src/menu/listitems.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
+import { Dashboard } from '@material-ui/icons';
 export const mainListItems = (
   <div>
     <ListItem button component="a" href="/app/dashboard/">
       <ListItemIcon>
-        <DashboardIcon />
+        <Dashboard />
       </ListItemIcon>
       <ListItemText primary="Dashboard" />
     </ListItem>

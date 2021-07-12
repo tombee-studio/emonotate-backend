0a1daaca16b24504fc611a478cd8fdfa267f499b
windows tomoya <tomomail1997@gmail.com>
2021-07-12 22:03:56 +0900

    clean file

--- frontend/src/menu/listitems.js
+++ frontend/src/menu/listitems.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import EditIcon from '@material-ui/icons/Edit';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 export const mainListItems = (
   <div>

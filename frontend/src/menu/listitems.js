af21e73258c4a1af70dc286a220074d2da854bee
windows tomoya <tomomail1997@gmail.com>
2021-07-25 09:09:07 +0900

    change icon

--- frontend/src/menu/listitems.js
+++ frontend/src/menu/listitems.js
@@ -3,13 +3,15 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import DashboardIcon from '@material-ui/icons/Dashboard';
+import HomeIcon from '@material-ui/icons/Home';
+
 export const mainListItems = (
   <div>
     <ListItem button component="a" href="/app/dashboard/">
       <ListItemIcon>
-        <DashboardIcon />
+        <HomeIcon />
       </ListItemIcon>
-      <ListItemText primary="ダッシュボード" />
+      <ListItemText primary="ホーム" />
     </ListItem>
   </div>
 );

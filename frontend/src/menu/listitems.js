31ee767b0f3bef20cf58b493ec7e7035bf1a34fc
windows tomoya <tomomail1997@gmail.com>
2021-07-25 09:09:36 +0900

    remove dependency

--- frontend/src/menu/listitems.js
+++ frontend/src/menu/listitems.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
 import HomeIcon from '@material-ui/icons/Home';
 
 export const mainListItems = (

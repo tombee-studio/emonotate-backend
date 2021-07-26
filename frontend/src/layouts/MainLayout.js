b09e3b2752faa54dfb864b11bd87199710dec495
windows tomoya <tomomail1997@gmail.com>
2021-07-26 18:26:10 +0900

    use main layout

--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -11,13 +11,12 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
-import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems } from '../menu/listitems';
-import ContentsList from '../components/dashboard/ContentsList';
+import SearchField from 'react-search-field';
 
 function Copyright() {
   return (

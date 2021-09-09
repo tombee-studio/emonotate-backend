ec7b56aae600baf6034133a5f51ae1b8c04b4cb5
Tomoya Bansho <tomomail1997@gmail.com>
2021-09-09 23:19:37 +0900

    add password reset

--- backend/apps/auth/urls.py
+++ backend/apps/auth/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, path
 
 from .views import *
 
 urlpatterns = [
-    url(r'login', auth.LoginView.as_view(), name='login'),
-    url(r'logout', auth.LogoutView.as_view(), { 'next_page': reverse_lazy('index') }, name='logout'),
+    path('', include('django.contrib.auth.urls')),
     url(r'signup', signup, name='signup')
 ]

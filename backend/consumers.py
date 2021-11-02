fcbf31f3683bc99424778577529c6ef5dbc8e67f
windows tomoya <tomomail1997@gmail.com>
2021-11-02 19:25:46 +0900

    change

--- backend/consumers.py
+++ backend/consumers.py
@@ -1,29 +1,7 @@
 from channels.generic.websocket import WebsocketConsumer
-from channels.db import database_sync_to_async
-import json
-
-from users.models import Log
 
 
 class ExperimentConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self._save_log(room=self.room_name, 
-            description="connected", 
-            state='connected')
-    
-    def disconnect(self, close_code):
-        self._save_log(room=self.room_name, 
-            description="disconnected", 
-            state='disconnected')
-        self.close()
-    
-    def _save_log(self, room, description, state, content_id=None, value_type_id=None):
-        Log.objects.create(
-            content=content_id,
-            value_type=value_type_id,
-            room=room,
-            description=description,
-            state=state
-        )
--- frontend/src/components/room-page/ObserverComponent.js
+++ frontend/src/components/room-page/ObserverComponent.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { 
     Avatar, 
-    Box, 
-    Grid, 
-    List, 
-    ListItem, 
-    ListItemAvatar, 
-    ListItemText,
-    Button } from "@material-ui/core";
-import { Chart } from "react-charts";
+    FormControl,
+    InputLabel,
+    Input,
+    Box,
+    Grid } from "@material-ui/core";
 import PersonIcon from "@material-ui/icons/Person";
-import videojs from "video.js";
 import "video.js/dist/video-js.css";
 import CurvesListAPI from "../../helper/CurvesListAPI";
 
@@ -18,42 +14,7 @@ class ObserverComponent extends React.Component {
     constructor(props) {
         super(props);
         const { request } = this.props;
-        this.request = request;
-        this.CONSTANT = {
-            video: 'video',
-            chart: 'chart',
-        };
-
-        this.videoJsOptions = {
-            autoplay: false,
-            controls: true,
-            sources: [{
-                src:  request.content.url,
-                type: request.content.data_type
-            }]
-        }
-
-        this.state = {
-            data: request.participants.map(user => {
-                return {
-                    'label': user.id,
-                    'data': []
-                }
-            }),
-            axes: request.participants.map((_, i) => {
-                if(i == 0) {
-                    return { primary: true, type: 'linear', position: 'bottom' };
-                } else {
-                    return { type: 'linear', position: 'left' };
-                }
-            }),
-            users: request.participants.map(user => {
-                return {
-                    'id': user.id,
-                    'data': user,
-                };
-            })
-        };
+        console.log(request);
     }
 
     download(exportJson) {
@@ -68,86 +29,15 @@ class ObserverComponent extends React.Component {
         document.body.removeChild(link);
     }
 
-    componentDidMount() {
-        this.player = videojs(this.videoNode, this.videoJsOptions);
-    }
-
-    updateUserState(userState) {
-        const { curve, user } = userState;
-        const curveData = this.state.data.concat();
-        const userData = this.state.users.concat();
-        const itemIndex = curveData.findIndex(item => item.label == user.id);
-        const userIndex = userData.findIndex(item => item.id == user.id);
-        curveData[itemIndex].data = curve;
-        userData[userIndex].data = user;
-        this.setState({
-            data: curveData,
-            users: userData
-        });
-    }
-
     render() {
+        const { request } = this.props;
         return (
-            <div>
-                <Box m={2}>
-                    <Grid container>
-                        <Grid item xs={5}>
-                        <div data-vjs-player>
-                            <video
-                                id={ this.CONSTANT.video }
-                                ref={ node => this.videoNode = node }
-                                height={ 240 }
-                                className="video-js" />
-                        </div>
-                        </Grid>
-                        <Grid item xs={7}>
-                            <div
-                                style={{
-                                    width: '100%',
-                                    height: '100%'
-                                }}>
-                                <Chart data={this.state.data} axes={this.state.axes} />
-                            </div>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <List>
-                                { this.state.users.map(user => {
-                                    return <ListItem key={user.id}>
-                                        <ListItemAvatar>
-                                            <Avatar>
-                                                <PersonIcon />
-                                            </Avatar>
-                                        </ListItemAvatar>
-                                        <ListItemText
-                                            primary={user.data.email}
-                                        />
-                                    </ListItem>
-                                })}
-                            </List>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Grid container>
-                                <Grid item xs={3}>
-                                    <Button onClick={ev => {
-                                        const api = new CurvesListAPI();
-                                        api.list({
-                                            'format': 'json',
-                                            'search': this.request.room_name,
-                                        })
-                                        .then(json => {
-                                            this.download(json);
-                                            alert("ダウンロードを終了しました");
-                                        })
-                                        .catch(err => {
-                                            alert(err);
-                                        });
-                                    }}>Download</Button>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </div>
+            <Box m={2}>
+                <FormControl>
+                    <InputLabel htmlFor="my-input">説明</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text" value={request.description} />
+                </FormControl>
+            </Box>
         );
     }
 };

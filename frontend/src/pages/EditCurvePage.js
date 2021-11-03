75dfae3bf5864d81378e118f04894a6405a6110e
Tomoya Bansho <tomomail1997@gmail.com>
2021-11-03 17:50:46 +0900

    fix bugs

--- frontend/src/pages/EditCurvePage.js
+++ frontend/src/pages/EditCurvePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
-import { CircularProgress } from '@material-ui/core';
+import { CircularProgress, Box } from '@material-ui/core';
 import CurvesListAPI from '../helper/CurvesListAPI';
 import EditCurveComponent from '../components/edit-curve-page/EditCurveComponent';
 

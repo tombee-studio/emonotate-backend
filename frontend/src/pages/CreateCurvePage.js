94f813f79ab018fe5815c5d93ec3e720ad492af1
Tomoya Bansho <tomomail1997@gmail.com>
2021-11-27 14:25:02 +0900

    初期値で分割するポイントを設定しました

--- frontend/src/pages/CreateCurvePage.js
+++ frontend/src/pages/CreateCurvePage.js
@@ -21,7 +21,7 @@ const CreateCurvePage = (props) => {
   const content_id = query.get('content');
   const value_type_id = query.get('value_type');
   const room_name = query.get('room') || `content=${content_id} value_type_id=${value_type_id}`;
-  const counts = query.get('counts') || 2;
+  const counts = query.get('counts') || 1;
 
   useEffect(() => {
     const apis = [];

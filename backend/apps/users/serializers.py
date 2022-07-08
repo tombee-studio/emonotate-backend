0c48a3738210de47b08e65e464f8841591bffd6d
Tomoya Bansho <tomomail1997@gmail.com>
2022-07-08 13:44:35 +0900

    force

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -124,7 +124,7 @@ class RequestSerializer(serializers.ModelSerializer):
         ret['content'] = ContentSerializer(Content.objects.get(pk=ret['content'])).data
         ret['value_type'] = ValueTypeSerializer(ValueType.objects.get(pk=ret['value_type'])).data
         ret['participants'] = [UserSerializer(User.objects.get(pk=pk)).data for pk in ret['participants']]
-        ret['is_able_to_send'] = datetime.now() > instance.expiration_date
+        ret['is_able_to_send'] = True
         if ret['questionaire'] != None:
             ret['questionaire'] = QuestionaireSerializer(Questionaire.objects.get(pk=ret['questionaire'])).data
         return ret

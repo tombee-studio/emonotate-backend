472c55628bbf8aded197adb02d06b373bcefa85c
Tomoya Bansho <gku14580@kwansei.ac.jp>
2023-02-05 01:18:25 +0900

    use access token

--- backend/views.py
+++ backend/views.py
@@ -13,8 +13,9 @@ from lazysignup.decorators import allow_lazy_user
 
 from django.http import JsonResponse
 
-from users.serializers import UserSerializer, RequestSerializer
+from users.serializers import UserSerializer, RequestSerializer, CurveSerializer
 from users.models import Request, Curve
+from django.views.generic import View, DetailView
 
 User = get_user_model()
 
@@ -47,55 +48,110 @@ def app(request):
     return render(request, template, context)
 
 
-@login_required
-def free_hand_view(request):
-    curve_json = Curve.get_empty_json()
-    if "request" in request.GET:
-        req = Request.objects.get(pk=int(request.GET.get("request")))
-        video_id = req.content.youtubecontent.video_id
-        curve_json["content"] = req.content.id
-        curve_json["value_type"] = req.value_type.id
-        curve_json["user"] = request.user.id
-        curve_json["locked"] = True
-        curve_json["room_name"] = req.room_name
-        request_json = RequestSerializer(req).data
+class FreeHandView(View):
+    def get(self, request, *args, **kwargs):
+        curve_json = Curve.get_empty_json()
+        if "request" in request.GET:
+            req = Request.objects.get(pk=int(request.GET.get("request")))
+            video_id = req.content.youtubecontent.video_id
+            curve_json["content"] = req.content.id
+            curve_json["value_type"] = req.value_type.id
+            curve_json["user"] = request.user.id
+            curve_json["locked"] = True
+            curve_json["room_name"] = req.room_name
+            request_json = RequestSerializer(req).data
+            context = {
+                "option": "new",
+                "video_id": video_id,
+                "has_google_form": req.google_form != None,
+                "request_model": req,
+                "curve_json": json.dumps(curve_json),
+                "request_json": json.dumps(request_json)
+            }
+            template = 'backend/free-hand.html'
+        else:
+            video_id = request.GET.get("video_id")
+            curve_json["content"] = None
+            curve_json["value_type"] = 1
+            curve_json["user"] = request.user.id
+            curve_json["locked"] = True
+            curve_json["room_name"] = request.GET.get("room_name", 
+                f"{request.GET.get('room_name')}")
+            context = {
+                "option": "new",
+                "video_id": video_id,
+                "curve_json": json.dumps(curve_json),
+                "request_json": {}
+            }
+            template = 'backend/free-hand.html'
+        response = render(request, template, context)
+        response.set_cookie('GCP_ACCESS_TOKEN', 'value')
+        return response
+
+
+class FreeHandDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        curve = Curve.objects.get(pk=pk)
+        curve_dict = CurveSerializer(curve).data
         context = {
-            "video_id": video_id,
-            "has_google_form": req.google_form != None,
-            "request_model": req,
-            "curve_json": json.dumps(curve_json),
-            "request_json": json.dumps(request_json)
-        }
-        template = 'backend/free-hand.html'
-        return render(request, template, context)
-    else:
-        video_id = request.GET.get("video_id")
-        curve_json["content"] = None
-        curve_json["value_type"] = 1
-        curve_json["user"] = request.user.id
-        curve_json["locked"] = True
-        curve_json["room_name"] = request.GET.get("room_name", 
-            f"{request.GET.get('room_name')}")
-        context = {
-            "video_id": video_id,
-            "curve_json": json.dumps(curve_json),
+            "option": "detail",
+            "video_id": curve.content.youtubecontent.video_id,
+            "curve_json": json.dumps(curve_dict),
             "request_json": {}
         }
         template = 'backend/free-hand.html'
-        return render(request, template, context)
+        response = render(request, template, context)
+        response.set_cookie('GCP_ACCESS_TOKEN', 'value')
+        return response
 
-@login_required
-def fold_line_view(request):
-    curve_json = Curve.get_empty_json()
-    video_id = request.GET.get("video_id")
-    curve_json["value_type"] = 1
-    curve_json["user"] = request.user.id
-    curve_json["locked"] = request.GET.get("locked", False)
-    curve_json["room_name"] = request.GET.get("room_name", 
-        f"{request.GET.get('room_name')}")
-    context = {
-        "video_id": video_id,
-        "curve_json": json.dumps(curve_json)
-    }
-    template = 'backend/fold-line.html'
-    return render(request, template, context)
+
+class FoldLineView(View):
+    def get(self, request, *args, **kwargs):
+        curve_json = Curve.get_empty_json()
+        if "request" in request.GET:
+            req = Request.objects.get(pk=int(request.GET.get("request")))
+            video_id = req.content.youtubecontent.video_id
+            curve_json["content"] = req.content.id
+            curve_json["value_type"] = req.value_type.id
+            curve_json["values"] = req.values
+            curve_json["user"] = request.user.id
+            curve_json["locked"] = True
+            curve_json["room_name"] = req.room_name
+            request_json = RequestSerializer(req).data
+            context = {
+                "video_id": video_id,
+                "has_google_form": req.google_form != None,
+                "request_model": req,
+                "curve_json": json.dumps(curve_json),
+                "request_json": json.dumps(request_json)
+            }
+            template = 'backend/fold-line.html'
+        else:
+            video_id = request.GET.get("video_id")
+            curve_json["value_type"] = 1
+            curve_json["user"] = request.user.id
+            curve_json["locked"] = request.GET.get("locked", False)
+            curve_json["room_name"] = request.GET.get("room_name", 
+                f"{request.GET.get('room_name')}")
+            context = {
+                "video_id": video_id,
+                "curve_json": json.dumps(curve_json)
+            }
+            template = 'backend/fold-line.html'
+        response = render(request, template, context)
+        response.set_cookie('GCP_ACCESS_TOKEN', 'value')
+        return response
+
+
+class FoldLineDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        curve = Curve.objects.get(pk=pk)
+        context = {
+            "video_id": curve.content.youtubecontent.video_id,
+            "curve_json": CurveSerializer(curve).data,
+            "request_json": {}
+        }
+        template = 'backend/free-hand.html'
+        response = render(request, template, context)
+        response.set_cookie('GCP_ACCESS_TOKEN', 'value')
+        return response
